be191d765740781f0bc7f80631b4c2b1f47e15aa
Oussama Ben Ouanes <91777481+OussemaBenOuanes@users.noreply.github.com>
2025-04-24 12:20:44 +0100

    django env

---
+++ Untitled-1.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+      <h1>Welcome to acoffeeforme!</h1>
+      <p>Your place to share and enjoy coffee moments.</p>
+    </div>
+  );
+}
+
+export default HomePage;
